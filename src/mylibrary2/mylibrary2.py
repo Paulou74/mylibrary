d32eb9c90209cd9e5215724a090f3dfd1a386b9f
Paulou74 <l.schiele@hotmail.fr>
2024-09-22 12:32:14 +0200

    feat(mylibrary2): add pandas as pd

--- src/mylibrary2/mylibrary2.py
+++ src/mylibrary2/mylibrary2.py
@@ -2,6 +2,4 @@ def hello_world():
     """Returns the string 'Hello World'."""
     return "Hello World"
 
-def hello_world2():
-    """Returns the string 'Hello World 2'."""
-    return "Hello World 2"
+import pandas as pd
